01c5967a21ecc66f630de2278320935e9424f384
matjoseph24 <mathew.a.joseph@accenture.com>
2025-03-28 10:18:46 +0000

    DL-15845: Upgraded dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   private val gatlingVersion = "3.6.1"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "performance-test-runner"   % "5.6.0" % Test,
+    "uk.gov.hmrc"             %% "performance-test-runner"   % "6.1.0" % Test,
     "io.gatling.highcharts"   %  "gatling-charts-highcharts" % gatlingVersion % Test,
     "io.gatling"              %  "gatling-test-framework"    % gatlingVersion % Test,
     "uk.gov.hmrc"             %% "tax-year"                  % "4.0.0"
