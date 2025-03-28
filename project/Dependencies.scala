a4383c966c734798936ec6a7c3c56b4b26c7d300
matjoseph24 <mathew.a.joseph@accenture.com>
2025-03-28 10:32:50 +0000

    DL-15845: Removed test-runner and highcharts dependencies as they are already provided by performance test runner

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,8 +6,6 @@ object Dependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "performance-test-runner"   % "6.1.0" % Test,
-    "io.gatling.highcharts"   %  "gatling-charts-highcharts" % gatlingVersion % Test,
-    "io.gatling"              %  "gatling-test-framework"    % gatlingVersion % Test,
     "uk.gov.hmrc"             %% "tax-year"                  % "4.0.0"
   )
 }
