31f41ec399b06442e997e6de8df9e7cb41aea0bd
matjoseph24 <mathew.a.joseph@accenture.com>
2025-09-04 10:50:58 +0100

    DL-17256: Updated syntax for string interpolation

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,8 +5,8 @@ object Dependencies {
   private val gatlingVersion = "3.6.1"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "performance-test-runner" % "6.2.0" % Test,
-    "uk.gov.hmrc" %% "tax-year"                % "6.0.0"
+    "uk.gov.hmrc" %% "performance-test-runner" % "6.1.0" % Test,
+    "uk.gov.hmrc" %% "tax-year"                % "4.0.0"
   )
 
 }
